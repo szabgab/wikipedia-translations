6a9de23f461793090d618b563d398940e19fe182
Gabor Szabo <gabor@szabgab.com>
2016-01-23 14:48:01 +0200

    separate code to load sql files

--- wt.pl
+++ wt.pl
@@ -4,14 +4,15 @@ use warnings;
 use 5.010;
 
 use Cwd qw(getcwd);
+use Data::Dumper qw(Dumper);
 use Web::Query;
 use LWP::Simple qw(getstore);
 use File::Temp qw(tempdir);
 use Getopt::Long qw(GetOptions);
 
-GetOptions(\my %opt, 'help', 'fetch') or usage();
+GetOptions(\my %opt, 'help', 'fetch', 'load') or usage();
 usage() if $opt{help};
-usage() if not @ARGV;
+usage('Need at least one language') if not @ARGV;
 my @languages = @ARGV;
 
 if ($opt{fetch}) {
@@ -44,11 +45,24 @@ if ($opt{fetch}) {
 }
 
 if ($opt{load}) {
-	system "mysql -u root -psecret wikipedia < a.sql";
-	system "mysql -u root -psecret wikipedia < b.sql";
+	foreach my $lang (@languages) {
+		say $lang;
+		my @files = glob "sql/${lang}wiki*.sql";
+		#print Dumper \@files;
+		die "Could not find files for $lang\n" if @files == 0;
+		die "Found only one file ($files[0]) for $lang\n" if @files == 1;
+		die "More than two files found for $lang: " . Dumper \@files if @files > 2;
+		foreach my $file (@files) {
+			system "mysql -u root -psecret wikipedia < $file";
+		}
+	}
 }
 
 sub usage {
+	my ($msg) = @_;
+	if ($msg) {
+		print "\n * $msg\n\n";
+	}
 	print <<"USAGE";
 Usage: $0 
            --fetch
