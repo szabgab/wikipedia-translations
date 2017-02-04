ae263e64dc46706b4a350b7b8d120833f22ebba4
Gabor Szabo <gabor@szabgab.com>
2017-02-04 14:51:48 +0200

    --all and --date added

--- wt.pl
+++ wt.pl
@@ -6,6 +6,7 @@ use 5.010;
 binmode(STDOUT, ":utf8");
 binmode(STDERR, ":utf8");
 
+use autodie;
 use Cwd qw(getcwd);
 use Data::Dumper qw(Dumper);
 use DBI;
@@ -13,161 +14,165 @@ use File::Copy qw(move);
 use File::Temp qw(tempdir);
 use Getopt::Long qw(GetOptions);
 use LWP::Simple qw(getstore);
-use Web::Query;
+#use Web::Query;
 use Path::Tiny qw(path);
-use JSON qw(decode_json from_json);
-use POSIX qw(strftime);
+use JSON qw(from_json);
+use POSIX ();
 
-my $json_str = path('languages.json')->slurp_utf8;
+my $languages_json = 'languages.json';
+my $json_str = do {
+    open my $fh, '<:encoding(UTF-8)', $languages_json;
+    local $/ = undef;
+    <$fh>;
+};
 #print($json_str);
-#exit;
 my $conf = from_json( $json_str, { utf8  => 0 } );
-
-#my $conf = decode_json $json_str;
 
 my $N = 250;
 
-my $date = strftime("%Y%m%d", localtime());
 
-GetOptions(\my %opt, 'help', 'fetch', 'load', 'html') or usage();
+GetOptions(\my %opt, 'help', 'fetch', 'load', 'html', 'date', 'all') or usage();
 usage() if $opt{help};
-usage('Need at least one language') if not @ARGV;
+usage() if not $opt{date};
 my @languages = @ARGV;
+if ($opt{all}) {
+    @languages = sort keys %$conf;
+}
+usage('Need at least one language') if not @languages;
 
 if ($opt{fetch}) {
-	my @links;
+    my @links;
 
-	# https://dumps.wikimedia.org/huwiki/20160111/huwiki-20160111-langlinks.sql.gz
-	#suwiki-20160111-
-	#suwiki-20160111-page.sql.gz 
-	# <li>2016-01-17 22:31:42 <a href="huwiki/20160111">huwiki</a>: <span class='done'>Dump complete</span></li>
-	
-	#wq('https://dumps.wikimedia.org/backup-index.html')->find('li')->each(sub {
-	#	my ($i, $elem) = @_;  # $elem is a Web::Query object
-	#    # 2016-01-14 21:23:04 snwiki: Dump complete
-	#	# TODO: 2017-02-01 15:28:06 arwiki: Partial dump
-	#	if ($elem->text =~ qr{^\d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d ([\w-]*wiki): Dump complete$}) {
-	#		my $link = $elem->find('a')->attr('href') // '';
-	#		#printf("li: %s %s\n", $elem->text, $link);  # link is: chwiki/20160111
-	#		push @links, $link;
-	#	}
-	#
-	#    #2016-01-17 22:31:42 <a href="huwiki/20160111">huwiki</a>: <span class='done'>Dump complete
-	#	#printf("li: %d %s\n", $i+1, $elem->tagname);  # $elem is a Web::Query object
-	#	
-	#});
-	#if (not @links) {
-	#	say "Could not find and link to download";
-	#}
-	
-	#foreach my $link (@links) {
-	#	my ($wiki, $date) = split /\//, $link;
-	#	next if not grep {$wiki eq "${_}wiki"} @languages;
-	#	say "Fetching $wiki files";
-	#	foreach my $file ("$wiki-$date-langlinks", "$wiki-$date-page") {
-	#		say "     $file";
-	#		getstore("https://dumps.wikimedia.org/$link/$file.sql.gz", "sql/$file.sql.gz");
-	#		system "gunzip sql/$file.sql.gz";
-	#	}
-	#}
-	foreach my $lang (@languages) {
-		say $lang;
-		# Links on the main page look like: /huwiki/20170201/
-		# https://dumps.wikimedia.org/hewiki/20170120/
-		# files to download:
-		# https://dumps.wikimedia.org/hewiki/20170201/hewiki-20170201-langlinks.sql.gz
-		# https://dumps.wikimedia.org/hewiki/20170201/hewiki-20170201-page.sql.gz
-		#my $url = "https://dumps.wikimedia.org/${lang}wiki/$date/";
-		#say $url;
-		mkdir 'sql' if not -e 'sql';
-		foreach my $type ("langlinks", "page") {
-			my $file = "${lang}wiki-$date-$type.sql.gz";
-			my $url = "https://dumps.wikimedia.org/${lang}wiki/$date/$file";
-			say $url;
-			my $start = time;
-			#say getstore($url, "sql/$file");
-			if (not -e"sql/$file") {
-				system "wget $url";
-				move $file, "sql/$file";
-				system "gunzip sql/$file";
-			}
-			my $end = time;
-			say "Elapsed: ", ($end-$start);
-		}
-	}
+    # https://dumps.wikimedia.org/huwiki/20160111/huwiki-20160111-langlinks.sql.gz
+    #suwiki-20160111-
+    #suwiki-20160111-page.sql.gz 
+    # <li>2016-01-17 22:31:42 <a href="huwiki/20160111">huwiki</a>: <span class='done'>Dump complete</span></li>
+    
+    #wq('https://dumps.wikimedia.org/backup-index.html')->find('li')->each(sub {
+    #   my ($i, $elem) = @_;  # $elem is a Web::Query object
+    #    # 2016-01-14 21:23:04 snwiki: Dump complete
+    #   # TODO: 2017-02-01 15:28:06 arwiki: Partial dump
+    #   if ($elem->text =~ qr{^\d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d ([\w-]*wiki): Dump complete$}) {
+    #       my $link = $elem->find('a')->attr('href') // '';
+    #       #printf("li: %s %s\n", $elem->text, $link);  # link is: chwiki/20160111
+    #       push @links, $link;
+    #   }
+    #
+    #    #2016-01-17 22:31:42 <a href="huwiki/20160111">huwiki</a>: <span class='done'>Dump complete
+    #   #printf("li: %d %s\n", $i+1, $elem->tagname);  # $elem is a Web::Query object
+    #   
+    #});
+    #if (not @links) {
+    #   say "Could not find and link to download";
+    #}
+    
+    #foreach my $link (@links) {
+    #   my ($wiki, $date) = split /\//, $link;
+    #   next if not grep {$wiki eq "${_}wiki"} @languages;
+    #   say "Fetching $wiki files";
+    #   foreach my $file ("$wiki-$date-langlinks", "$wiki-$date-page") {
+    #       say "     $file";
+    #       getstore("https://dumps.wikimedia.org/$link/$file.sql.gz", "sql/$file.sql.gz");
+    #       system "gunzip sql/$file.sql.gz";
+    #   }
+    #}
+    foreach my $lang (@languages) {
+        say $lang;
+        # Links on the main page look like: /huwiki/20170201/
+        # https://dumps.wikimedia.org/hewiki/20170120/
+        # files to download:
+        # https://dumps.wikimedia.org/hewiki/20170201/hewiki-20170201-langlinks.sql.gz
+        # https://dumps.wikimedia.org/hewiki/20170201/hewiki-20170201-page.sql.gz
+        #my $url = "https://dumps.wikimedia.org/${lang}wiki/$date/";
+        #say $url;
+        mkdir 'sql' if not -e 'sql';
+        foreach my $type ("langlinks", "page") {
+            my $file = "${lang}wiki-$opt{date}-$type.sql.gz";
+            my $url = "https://dumps.wikimedia.org/${lang}wiki/$opt{date}/$file";
+            say $url;
+            my $start = time;
+            #say getstore($url, "sql/$file");
+            if (not -e"sql/$file") {
+                system "wget $url";
+                move $file, "sql/$file";
+                system "gunzip sql/$file";
+            }
+            my $end = time;
+            say "Elapsed: ", ($end-$start);
+        }
+    }
 }
 
 foreach my $lang (@languages) {
-	if ($opt{load}) {
-		say "Loading language $lang";
-		my @files = glob "sql/${lang}wiki*.sql";
-		#print Dumper \@files;
-		if (@files == 0) {
-			warn "Could not find files for $lang\n";
-			next;
-		}
-		if (@files == 1) {
-			warn "Found only one file ($files[0]) for $lang\n";
-			warn next;
-		}
-		if (@files > 2) {
-			warn "More than two files found for $lang: " . Dumper \@files;
-			next;
-		}
-		foreach my $file (@files) {
-			system "mysql -u root -psecret wikipedia < $file";
-		}
-	}
-	if ($opt{html}) {
-		say "Creating HTML for language $lang";
-		generate_html($lang);
-	}
+    if ($opt{load}) {
+        say "Loading language $lang";
+        my @files = glob "sql/${lang}wiki*.sql";
+        #print Dumper \@files;
+        if (@files == 0) {
+            warn "Could not find files for $lang\n";
+            next;
+        }
+        if (@files == 1) {
+            warn "Found only one file ($files[0]) for $lang\n";
+            warn next;
+        }
+        if (@files > 2) {
+            warn "More than two files found for $lang: " . Dumper \@files;
+            next;
+        }
+        foreach my $file (@files) {
+            system "mysql -u root -psecret wikipedia < $file";
+        }
+    }
+    if ($opt{html}) {
+        say "Creating HTML for language $lang";
+        generate_html($lang);
+    }
 }
 
 
 
 #if ($opt{html}) {
-#	my $html = "<ul>\n";
-#	foreach my $lang (sort keys %$conf) {
-#		$html .= qq{    <li><a href="/wikipedia/$lang">$conf->{$lang}{language}</a></li>\n};
-#	}
-#	$html .= "</ul>\n";
-#	path('list.txt')->spew_utf8($html);
+#   my $html = "<ul>\n";
+#   foreach my $lang (sort keys %$conf) {
+#       $html .= qq{    <li><a href="/wikipedia/$lang">$conf->{$lang}{language}</a></li>\n};
+#   }
+#   $html .= "</ul>\n";
+#   path('list.txt')->spew_utf8($html);
 #}
 
 
 sub generate_html {
-	my ($wiki) = @_;
+    my ($wiki) = @_;
 
-	my $url = "https://$wiki.wikipedia.org";
-	my $dbh = DBI->connect('DBI:mysql:database=wikipedia;', 'root', 'secret');
+    my $url = "https://$wiki.wikipedia.org";
+    my $dbh = DBI->connect('DBI:mysql:database=wikipedia;', 'root', 'secret');
 #AND page_len > 500 
-	my $sth = $dbh->prepare(q{
-		SELECT page_title, page_id, page_len FROM page WHERE page_namespace=0 AND page_is_redirect=0 AND page_is_new=0 AND page_id NOT IN
+    my $sth = $dbh->prepare(q{
+        SELECT page_title, page_id, page_len FROM page WHERE page_namespace=0 AND page_is_redirect=0 AND page_is_new=0 AND page_id NOT IN
                           (SELECT ll_from FROM langlinks WHERE ll_lang='en') ORDER BY page_len DESC LIMIT ?
-			});
-	$sth->execute($N);
-	my $html = "<ul>\n";
-	while (my $h = $sth->fetchrow_hashref) {
-		$html .= qq{<li><a href="$url/wiki/$h->{page_title}">$h->{page_title}  ($h->{page_len})</a></li>\n};
-	}
+            });
+    $sth->execute($N);
+    my $html = "<ul>\n";
+    while (my $h = $sth->fetchrow_hashref) {
+        $html .= qq{<li><a href="$url/wiki/$h->{page_title}">$h->{page_title}  ($h->{page_len})</a></li>\n};
+    }
 
-	my ($total_pages) = $dbh->selectrow_array(q{SELECT COUNT(*) FROM page WHERE page_namespace=0 and page_is_redirect=0 AND page_is_new=0});
-	my ($no_english) = $dbh->selectrow_array(q{SELECT COUNT(page_title) FROM page WHERE page_namespace=0 AND page_is_redirect=0 AND page_is_new=0 AND page_id NOT IN
+    my ($total_pages) = $dbh->selectrow_array(q{SELECT COUNT(*) FROM page WHERE page_namespace=0 and page_is_redirect=0 AND page_is_new=0});
+    my ($no_english) = $dbh->selectrow_array(q{SELECT COUNT(page_title) FROM page WHERE page_namespace=0 AND page_is_redirect=0 AND page_is_new=0 AND page_id NOT IN
                           (SELECT ll_from FROM langlinks WHERE ll_lang='en')});
 
 
-	$html .= "</ul>";
-		#print Dumper $h;
-		#<STDIN>;
-use DateTime::Tiny;
+    $html .= "</ul>";
+        #print Dumper $h;
+        #<STDIN>;
 
-	my $date = DateTime::Tiny->now;
-	my $rtl = $conf->{$wiki}{rtl} ? q{ class="rtl"} : '';
+    my $timestamp = POSIX::strftime("%Y%m%dT%H:%M:%S", localtime());
+    my $rtl = $conf->{$wiki}{rtl} ? q{ class="rtl"} : '';
 
-	my $out = <<"HTML";
+    my $out = <<"HTML";
 =title Wikipedia: $conf->{$wiki}{language} ($wiki)
-=timestamp $date
+=timestamp $timestamp
 =indexes wikipedia
 =status show
 =books wikipedia
@@ -182,8 +187,8 @@ use DateTime::Tiny;
 <!-- This is a generated file. Do not edit manually! -->
 <style>
 .rtl {
-	direction: rtl;
-	text-align: right;
+    direction: rtl;
+    text-align: right;
 }
 </style>
 
@@ -194,7 +199,7 @@ Here we you can see the $N largest articles in that don't have links to their En
 These articles either need an interwiki link to the English version of this page (and one from English back to this page),
 or they need to be translated to English first and then they need the link to that article.
 <p>
-Last updated at $date
+Last updated at $timestamp
 <p>
 
 <div$rtl>
@@ -203,27 +208,33 @@ $html
 
 HTML
 
-	path("$wiki.txt")->spew($out);
+    open my $fh, '>:encoding(UTF-8)', "$wiki.txt";
+    print $fh $out;
+    close $fh;
 }
 
 
 
 sub usage {
-	my ($msg) = @_;
-	if ($msg) {
-		print "\n * $msg\n\n";
-	}
-	my $languages = join " ", sort keys %$conf;
-	print <<"USAGE";
+    my ($msg) = @_;
+    if ($msg) {
+        print "\n * $msg\n\n";
+    }
+    my $languages = join " ", sort keys %$conf;
+    print <<"USAGE";
 Usage: $0 
+           --date YYYYMMDD
            --fetch
            --load
            --html
+
+           --all
            LANGUAGES    ($languages)
 
            --help  (this help)
 USAGE
-	exit;
+    exit;
 }
 
+# vim: expandtab
 
